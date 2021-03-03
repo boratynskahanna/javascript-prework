0e55eb7696a4daaaeb2fb34b4bbb23c65f145435
Hanna Boratynska <boratynskahanna@gmail.com>
2021-03-03 15:51:10 +0400

    Add options for player move

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+// Komputer
+
 let randomNumber = Math.floor(Math.random() * 3 + 1);
 
 console.log('Wylosowana liczba to: ' + randomNumber);
@@ -14,3 +16,20 @@ if(randomNumber == 1){
 
 printMessage('Mój ruch to: ' + computerMove);
 
+// Gracz
+
+let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+
+console.log('Gracz wpisał: ' + playerInput);
+
+let playerMove = 'nieznany ruch';
+
+if(playerInput == '1'){
+    playerMove = 'kamień';
+}   else if(playerInput == '2'){
+    playerMove = 'papier';
+}   else if(playerInput == '3'){
+    playerMove = 'nożyce';
+}
+
+printMessage('Twój ruch to: ' + playerMove);
